2ebcd663fdc53e437d854ccfedc14092db2b9fa4
navi1603 <ukrainec1603@gmail.com>
2023-03-05 22:50:24 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+namespace ChatBot
+{
+    public class ChatBot
+    {
+        public static void Main (string[] args)
+        {
+            var tutor = new Tutor();
+            tutor.AddWord("hello", "привет");
+            tutor.AddWord("dog", "собака");
+            tutor.AddWord("cat", "кот");
+
+            while (true)
+            {
+                var word = tutor.GetRandomEngWord();
+                Console.WriteLine($"Как переводится слово: {word}?");
+                var userAnswer = Console.ReadLine();
+                if (tutor.CheckWord(word, userAnswer))
+                    Console.WriteLine("Привильно!");
+                else
+                {
+                    var correctAnswer = tutor.Translate(word);
+                    Console.WriteLine($"Неверною Правильный ответ: {correctAnswer}");
+                }
+                   
+
+            }
+        }
+    }
+}
---
+++ Tutor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatBot
+{
+    public class Tutor
+    {
+        private Dictionary<string, string> _dic = new Dictionary<string, string>();
+        private Random _rand = new Random();
+
+        public void AddWord(string eng, string rus)
+        {
+           _dic.Add(eng, rus);
+        }
+
+        public bool CheckWord(string eng, string rus)
+        {
+            var answer = _dic[eng];
+            return answer.ToLower() == rus.ToLower();
+        }
+
+        public string Translate (string eng)
+        {
+            if(_dic.ContainsKey(eng))
+                return _dic[eng];
+            else 
+                return null;
+        }
+
+        public string GetRandomEngWord()
+        {
+            var r = _rand.Next(0, _dic.Count);
+            var keys = new List<string>(_dic.Keys);
+            return keys[r];
+        }
+    }
+}
